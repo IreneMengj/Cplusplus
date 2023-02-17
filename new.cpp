65cd0181a4949b2c61daa77f528375a00aef97a0
Irene <88880169+IreneMengj@users.noreply.github.com>
2023-02-17 11:15:07 +0000

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int * func()
+{
+    int *p=new int(10);
+    return p;
+}
+
+int main(){
+    int *p = func();
+    cout<<*p<<endl;
+    delete p;
+    cout<<*p<<endl;
+}
